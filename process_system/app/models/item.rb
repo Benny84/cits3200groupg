4fd2fbf826c06d04e87b12d19ea99476179f4a63
raix2 <20744555@student.uwa.edu.au>
2013-10-01 12:52:58 +0800

    fixed permissions of attributes

--- process_system/app/models/item.rb
+++ process_system/app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ActiveRecord::Base
-    attr_accessible :title, :body, :task, :complete
+    attr_accessible :pid, :title, :body, :task, :complete
     has_many :children, class_name: "Item", foreign_key: "pid"
     belongs_to :parent, class_name: "Item"
     has_many :triggers
--- process_system/app/models/trigger.rb
+++ process_system/app/models/trigger.rb
@@ -1,5 +1,6 @@
 class Trigger < ActiveRecord::Base
+    attr_accessible :item_id, :date_trigger
     belongs_to :item
     has_many :emails, through: :trigger_email
-    validates :date, presence: true
+    validates :date_trigger, presence: true
 end
