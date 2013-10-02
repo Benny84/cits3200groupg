b041229fffbfd59de3cc3496beba9ae88edf4242
raix2 <20744555@student.uwa.edu.au>
2013-10-02 12:43:32 +0800

    Fixed models with right permissions and associations

--- app/models/trigger.rb
+++ app/models/trigger.rb
@@ -1,6 +1,7 @@
 class Trigger < ActiveRecord::Base
     attr_accessible :item_id, :date_trigger
     belongs_to :item
-    has_many :emails, through: :trigger_email
+    has_many :trigger_emails
+    has_many :emails, :through => :trigger_email
     validates :date_trigger, presence: true
 end
