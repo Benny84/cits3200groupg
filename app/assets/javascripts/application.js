624acebce5f41dd397c4f386b0a6ae3c72bd5401
Eric Tan <20925931@student.uwa.edu.au>
2013-10-08 02:29:46 +0800

    Email form done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,14 @@ function hideAllForms() {
 	$('#set-email-form').hide();
 }
 
+function cancelItemForm() {
+	$('#add-item-form').hide();
+}
+
+function cancelEmailForm() {
+	$('#set-email-form').hide();
+}
+
 $(function () {
 	hideAllForms();
 	
@@ -31,40 +39,6 @@ $(function () {
         $('#parent_id').val($(this).attr('name'));
 		$('#set-email-form').insertAfter($('#item' + $(this).attr('name')));
 		$('#set-email-form').show();
-	});
-	
-	// Add Item Form Options	
-	$('#add-item-cancel').click(function() {
-		$('#add-item-form').hide();
-	});
-	
-	$('#add-item-clear').click(function() {
-		document.getElementById("add-item-form").reset();
-	});
-	
-	// Add Root Item Form Options		
-	$('#add-root-item-clear').click(function() {
-		document.getElementById("add-root-item-form").reset();
-	});
-	
-	// Add Email Reminder Form Options
-	/* this is set to be deleted, not sure if raissa wants to use any of it though
-	$('#set-email-confirm').click(function() { 
-		var date = document.getElementById("datepicker").value;
-		var recip = document.getElementById("email-recip").value;
-		
-		$('#currentItem').append("<p class = 'emailReminder'>Email to " + recip + " at " + date + " 00:00:00 UTC</p>");
-		
-		document.getElementById("set-email-form").reset();
-		$('#set-email-form').hide();
-	});*/
-	
-	$('#set-email-cancel').click(function() {
-		$('#set-email-form').hide();
-	});
-	
-	$('#set-email-clear').click(function() {
-		document.getElementById("set-email-form").reset();
 	});
 	
 	//Date picker jQuery
