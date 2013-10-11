fddb66c063e8b72607921a07a2ad9509ab2c3477
Eric Tan <20925931@student.uwa.edu.au>
2013-10-12 06:16:06 +0800

    Removed time portion of the date picker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,61 +26,9 @@ function cancelEmailForm() {
 	$('#set-email-form').hide();
 }
 
-function getPageItemId() {
-	//grab it from the URL
-	var curId = document.location.pathname.match(/[^\/]+$/)[0];	
-	//check if its an editing page, if so get id from appropriate place
-	if (curId == "edit") {
-		curId = $("#getThis").attr('name');
-	}
-	return curId;
-}
-
-// function constructHierarchy() {	
-// 	//firstly, hide every node in the hierarchy
-// 	//SILLY WAY TO HIDE EVERY NODE...
-// 	for (var i = 1; i < 500; i++){
-// 		$("#tree" + i).hide();
-// 	}
-	
-// 	//then show the root nodes
-// 	$('*[name = ""]').show();
-	
-// 	//grab current item id
-// 	var curId = getPageItemId();
-// 	//then firstly show the current items direct children
-// 	//$('*[name = curId]').show(); //THIS DOESNT WORK
-	
-// 	//make sure we aren't on the index page or on a new root item/email page
-// 	if (curId != "items" && curId != "new") {	
-// 		//then show the current node, and all of its direct parents until the root
-// 		while (curId != "") {
-// 			$("#tree" + curId).show();
-// 			$("#tree" + curId).attr("style", "list-style-type:disc");
-// 			curId = $("#tree" + curId).attr('name');
-// 		}
-	
-// 		//re-initialise curId (this can probably be done better)
-// 		curId = getPageItemId();
-		
-// 		//bold the current item
-// 		$("#tree" + curId).attr("style", "font-weight: bold; list-style-type:disc");
-		
-// 		//show children...ideally done before but oh well
-// 		for (var i = 1; i < 500; i++) { //THIS IS STUPID
-// 			if ($("#tree" + i).attr('name') == curId) {
-// 				$("#tree" + i).show();
-// 				$("#tree" + i).attr("style", "font-weight: normal");
-// 			}
-// 		}
-// 	}	
-// }
-
 $(function () {
 	//hide the add item and email forms at page load
 	hideAllForms();
-	//construct the hierarchy according to what page you're currently on
-	//constructHierarchy();
 	
 	$('#add-item').click(function() {
 		hideAllForms();
@@ -96,5 +44,5 @@ $(function () {
 	
 	//Date picker jQuery
 	var dateToday = new Date();
-	$('#datetimepicker').datetimepicker({ dateFormat: "dd/mm/yy", minDate: dateToday });
+	$('#datepicker').datepicker({ dateFormat: "dd/mm/yy", minDate: dateToday });
 });
