68a450329befe86e80e66d52d0afa9f225bf3af8
Eric Tan <20925931@student.uwa.edu.au>
2013-10-14 05:00:09 +0800

    Current item path is now indicated with filled circles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,9 +26,41 @@ function cancelEmailForm() {
 	$('#set-email-form').hide();
 }
 
+function getPageItemId() {
+	//grab it from the URL
+	var curId = document.location.pathname.match(/[^\/]+$/)[0];	
+	//check if its an editing page, if so get id from appropriate place
+	if (curId == "edit") {
+		curId = $("#itemId").attr('name');
+	}
+	return curId;
+}
+
+function indicateItemPath() {
+	//grab current item id
+	var curId = getPageItemId();
+	
+	//make sure we are on an item page
+	if (!isNaN(curId)) {
+		var i = 0;
+		//then indicate the current node, and all of its direct parents until the root
+		while (curId != "") {
+			$("#tree" + curId).attr("style", "list-style-type: disc");
+			curId = $("#tree" + curId).attr('name');
+			
+			//fail safe - in case of new pages being numbered do not have an item id equal to the page
+			i++;
+			if (i == 50) //50 levels deep hierarchy...seems safe enough 
+				break;
+		}
+	}
+}
+
 $(function () {
 	//hide the add item and email forms at page load
 	hideAllForms();
+	//indicate the current item path
+	indicateItemPath();
 	
 	$('#add-item').click(function() {
 		hideAllForms();
